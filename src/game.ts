7fe605464914762cadff4276db2256b7c2c896b3
Christian Voigt <1pxsolidblack@gmail.com>
2021-12-03 19:15:00 +0100

    further simplified example

--- src/game.ts
+++ src/game.ts
@@ -6,25 +6,15 @@ export default class Demo extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("tiles", "assets/tilesets/tuxmon-sample-32px-extruded.png");
-    this.load.image("debug", "assets/tilesets/debug.png");
-    this.load.spritesheet("food", "assets/tilesets/food-OCAL.png", {
-      frameWidth: 32,
-      frameHeight: 32,
-    });
+    this.load.image("tiles2", "assets/tilesets/debug.png");
     this.load.tilemapTiledJSON("map", "assets/tilemaps/tuxemon-town.json");
   }
 
   create() {
     const map = this.make.tilemap({ key: "map" });
-    const tileset1 = map.addTilesetImage(
-      "tuxmon-sample-32px-extruded",
-      "tiles"
-    );
-    const tileset2 = map.getTileset("food-OCAL");
-    const tileset3 = map.addTilesetImage("debug", "debug");
-    const myNewLayer = map.createLayer("MyNewLayer", [tileset3], 0, 0);
-    const tileGid = tileset3.firstgid;
+    const tileset2 = map.addTilesetImage("debug", "tiles2");
+    const myNewLayer = map.createLayer("MyNewLayer", [tileset2], 0, 0);
+    const tileGid = tileset2.firstgid;
     map.putTileAt(tileGid, 5, 5, false, myNewLayer);
   }
 }
